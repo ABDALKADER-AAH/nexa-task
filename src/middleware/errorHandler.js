d0544cba5b7d16e8205cba3a8a753fdc508f7ed9
ABDALKADER-AAH <79111470+ABDALKADER-AAH@users.noreply.github.com>
2025-09-28 17:14:50 +0300

    Update

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -4,8 +4,7 @@ const errorHandler = (err, req, res, next) => {
   // Log the error for debugging purposes
   console.error(err.stack);
 
-  // Determine the status code. If the error has a specific status, use it.
-  // Otherwise, default to 500 (Internal Server Error).
+  // default to 500 (Internal Server Error).
   const statusCode = err.statusCode || 500;
 
   // Send a structured error response
